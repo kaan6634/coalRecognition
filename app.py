2b712f27f372c4524989195d31659cf292693e36
Kaan Yılmaz <71845829+kaan6634@users.noreply.github.com>
2024-07-24 15:29:04 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, send_from_directory, url_for
+from flask import Flask, request, render_template, send_from_directory, url_for
 from PIL import Image
 import numpy as np
 import tensorflow as tf
@@ -9,7 +9,6 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 
 model = tf.keras.models.load_model('coal_recognition_model.h5')
 
-
 def preprocess_image(image, target_size):
     if image.mode == 'RGBA':
         image = image.convert('RGB')
@@ -19,108 +18,11 @@ def preprocess_image(image, target_size):
     image = np.expand_dims(image, axis=0)
     return image
 
-
 class_names = {0: "antrasit komuru", 1: "linyit kömürü", 2: "taş kömürü"}
-
-html_template = '''
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coal Recognition</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f4f4f9;
-            color: #333;
-            margin: 0;
-            padding: 20px;
-        }
-        h1 {
-            text-align: center;
-            color: #444;
-        }
-        form {
-            max-width: 500px;
-            margin: 20px auto;
-            padding: 20px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-            background-color: #fff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-        input[type="file"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-        input[type="submit"] {
-            display: block;
-            width: 100%;
-            padding: 10px;
-            background-color: #5cb85c;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            font-size: 16px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #4cae4c;
-        }
-        .image-container {
-            max-width: 500px;
-            margin: 20px auto;
-            padding: 20px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-            background-color: #fff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-        .image-container img {
-            max-width: 100%;
-            height: auto;
-            display: block;
-            margin: 10px 0;
-            border-radius: 4px;
-        }
-        .image-container h2 {
-            color: #5cb85c;
-        }
-        .image-container p {
-            color: #555;
-        }
-    </style>
-</head>
-<body>
-    <h1>Upload Coal Images for Recognition</h1>
-    <form method="post" action="/upload" enctype="multipart/form-data">
-        <input type="file" name="file" multiple accept="image/*">
-        <input type="submit" value="Upload">
-    </form>
-    {% if predictions %}
-        {% for result in predictions %}
-            <div class="image-container">
-                <h2>Predicted Class: {{ result.class_name }}</h2>
-                <p>Confidence: {{ result.confidence }}</p>
-                <img src="{{ result.image_url }}" alt="Uploaded Image">
-            </div>
-        {% endfor %}
-    {% endif %}
-</body>
-</html>
-
-'''
-
 
 @app.route('/')
 def index():
-    return render_template_string(html_template)
-
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -143,13 +45,11 @@ def upload():
                     'image_url': url_for('uploaded_file', filename=file.filename)
                 })
 
-    return render_template_string(html_template, predictions=predictions)
-
+    return render_template('index.html', predictions=predictions)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
